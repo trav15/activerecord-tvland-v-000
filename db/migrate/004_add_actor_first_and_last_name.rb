e2805d97ca49701d4e3b0fda2ef2960bafd07f01
Travis T <travistokuyama@gmail.com>
2018-11-07 20:15:59 +0000

    automatically backed up by learn

--- db/migrate/004_add_actor_first_and_last_name.rb
+++ db/migrate/004_add_actor_first_and_last_name.rb
@@ -1,6 +1,8 @@
 class AddActorFirstAndLastName < ActiveRecord::Migration[4.2]
-  add_column :actors do |a|
-    a.string :first_name
-    a.string :last_name
+  def change
+    create_table :actors do |t|
+      t.string :first_name
+      t.string :last_name
+    end
   end
 end
